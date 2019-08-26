97b92331aa4b68b664ae7fecde4d1cc53422ab71
anshuman1998 <25300718+anshuman1998@users.noreply.github.com>
2019-08-26 12:00:18 +0530

    Update q18.py

--- q18.py
+++ q18.py
@@ -1,9 +1,24 @@
-import osimport numpy as npimport subprocess
+import os
+import numpy as np
+import subprocess
 
 from astropy.table import Table
 
 #Run Cloudy to generate test observations
-UVB_Q = 18 #Q18 Quasar Sourcedirname='/Users/anshumanacharya/Downloads/c17.01/source/pyprog'+'{:.0f}'.format(UVB_Q)run='/Users/anshumanacharya/Downloads/c17.01/source/cloudy.exe'hden= -4z = 0.1stcolden= 14if not os.path.exists(dirname):     os.makedirs(dirname)os.chdir(dirname)filena = dirname + '/prog'+'{:.0f}'.format(UVB_Q)+'.in'f=open(filena,"w+")f.write("TABLE KS18 redshift = 0.2 [scale= 1][Q="+"{:.0f}".format(UVB_Q)+"] 
+
+UVB_Q = 18 #Q18 Quasar Source
+dirname='/Users/anshumanacharya/Downloads/c17.01/source/pyprog'+'{:.0f}'.format(UVB_Q)
+
+run='/Users/anshumanacharya/Downloads/c17.01/source/cloudy.exe'
+hden= -4
+z = 0.1
+stcolden= 14
+if not os.path.exists(dirname):
+     os.makedirs(dirname)
+os.chdir(dirname)
+filena = dirname + '/prog'+'{:.0f}'.format(UVB_Q)+'.in'
+f=open(filena,"w+")
+f.write("TABLE KS18 redshift = 0.2 [scale= 1][Q="+"{:.0f}".format(UVB_Q)+"] 
 \nhden" + "{:.0f}".format(hden)+ "\nmetals" + "{:.2f}".format(z)+ " log 
 \nelement helium abundance 0.081632653 linear \nstop column density"+
 "{:.1f}".format(stcolden)+" neutral H 
@@ -31,13 +46,22 @@ from astropy.table import Table
 \nend")
 
 #Saving column density values for 20 metals
-f.close()process = subprocess.Popen([run, "prog"+"{:.0f}".format(UVB_Q)+".in"], stdout =subprocess.PIPE)process.stdout.read()data=np.genfromtxt('/Users/anshumanacharya/Downloads/c17.01/source/pyprog'+
+
+f.close()
+process = subprocess.Popen([run, "prog"+"{:.0f}".format(UVB_Q)+".in"], stdout =subprocess.PIPE)
+process.stdout.read()
+
+data=np.genfromtxt('/Users/anshumanacharya/Downloads/c17.01/source/pyprog'+
 '{:.0f}'.format(UVB_Q)+'/prog'+'{:.0f}'.format(UVB_Q)+'.spC')
 
 #Save Column density data on astropy table
-t=Table(data,names=['CII','CIII','CIV','CV','MgII','NeVIII',
+
+t=Table(data,names=['CII','CIII','CIV','CV','MgII','NeVIII',
 'NII','NIII','NIV','OII','OIII','OIV','OV','OVI','SiII',
 'SiIII','SiIV','SIV','SV','SVI'],
 meta={'UVB_Q':UVB_Q, 'metallicity': z,'n_H':hden,'log(stop.colden)':stcolden})
-t.write('/Users/anshumanacharya/Downloads/c17.01/source/pyprog'+'{:.0f}'.format(UVB_Q)+'/colden.fits')read_obs=Table.read('/Users/anshumanacharya/Downloads/c17.01/source/pyprog'+
-'{:.0f}'.format(UVB_Q)+'/colden.fits')read_obs.meta
+
+t.write('/Users/anshumanacharya/Downloads/c17.01/source/pyprog'+'{:.0f}'.format(UVB_Q)+'/colden.fits')
+read_obs=Table.read('/Users/anshumanacharya/Downloads/c17.01/source/pyprog'+
+'{:.0f}'.format(UVB_Q)+'/colden.fits')
+read_obs.meta
