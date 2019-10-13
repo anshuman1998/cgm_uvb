dec986a80c89ef4ba9955a45e8ea0a9e53f55e12
anshuman1998 <25300718+anshuman1998@users.noreply.github.com>
2019-10-13 18:45:43 +0530

    Update form_obs_col_ratios.py

--- form_obs_col_ratios.py
+++ form_obs_col_ratios.py
@@ -16,9 +16,29 @@ filena = dirname + 'prog'+'{:.0f}'.format(UVB_Q)+'_Oct12.in'
 f=open(filena,"w+")
 
 f.write("TABLE KS18 redshift = 0.2 [scale= 1][Q= "+"{:.0f}".format(UVB_Q)+"] \nhden " + 
- "{:.0f}".format(hden)+ "\nmetals " + "{:.2f}".format(z)+ " log \nelement helium abundance 0.081632653 linear \nstop column density "+"{:.1f}".format(stcolden)
- +
-" neutral H \nconstant temperature, t=1e4 K [linear] \nsave species column density \".spC\"no hash \n\"C+\" \n\"C+2\" \n\"C+3\" \n\"N+\" \n\"N+2\" \n\"N+3\" \n\"N+4\" \n\"O\" \n\"O+\"  \n\"O+2\" \n\"O+3\" \n\"O+4\" \n\"O+5\" \n\"S+3\" \n\"S+4\" \n\"S+5\" \n\"Si+\" \n\"Si+2\" \n\"Si+3\" \nend") 
+ "{:.0f}".format(hden)+ "\nmetals " + "{:.2f}".format(z)+ " log \nelement helium abundance 0.081632653 linear 
+        \nstop column density "+"{:.1f}".format(stcolden)
+ +" neutral H \nconstant temperature, t=1e4 K [linear] \nsave species column density \".spC\" no hash 
+        \n\"C+\" 
+        \n\"C+2\" 
+        \n\"C+3\" 
+        \n\"N+\" 
+        \n\"N+2\" 
+        \n\"N+3\" 
+        \n\"N+4\" 
+        \n\"O\" 
+        \n\"O+\"  
+        \n\"O+2\" 
+        \n\"O+3\" 
+        \n\"O+4\" 
+        \n\"O+5\" 
+        \n\"S+3\" 
+        \n\"S+4\" 
+        \n\"S+5\" 
+        \n\"Si+\" 
+        \n\"Si+2\" 
+        \n\"Si+3\" 
+        \nend") 
 f.close()
 
 process = subprocess.Popen([run, "prog"+"{:.0f}".format(UVB_Q)+"_Oct12.in"], 
@@ -26,7 +46,8 @@ stdout =subprocess.PIPE)
 process.stdout.read()
 
 from astropy.table import Table
-data=np.genfromtxt('/Users/anshumanacharya/Downloads/c17.01/source/pyprog'+'{:.0f}'.format(UVB_Q) +'/Final/prog'+'{:.0f}'.format(UVB_Q)+'_Oct12.spC')
+data=np.genfromtxt('/pyprog'+'{:.0f}'.format(UVB_Q) +
+                   '/Final/prog'+'{:.0f}'.format(UVB_Q)+'_Oct12.spC')
 t=Table(data,names=['CII', 'CIII', 'CIV', 'NII', 'NIII', 
                     'NIV', 'NV', 'OI', 'OII', 'OIII', 'OIV', 
                     'OV', 'OVI', 'SIV', 'SV', 'SVI', 'SiII', 
@@ -40,7 +61,8 @@ read_obs.meta
 
 #Ratios to use
 
-colratios=['CII/CIII', 'CIII/CIV', 'NII/NIII', 'NIII/NIV','NIV/NV','OI/OII','OII/OIII','OIII/OIV','OIV/OV','OV/OVI','SIV/SV','SV/SVI','SiII/SiIII','SiIII/SiIV']
+colratios=['CII/CIII', 'CIII/CIV', 'NII/NIII', 'NIII/NIV','NIV/NV','OI/OII','OII/OIII',
+           'OIII/OIV','OIV/OV','OV/OVI','SIV/SV','SV/SVI','SiII/SiIII','SiIII/SiIV']
 
 ratio_obs=[]
 for i in range(len(data)):
@@ -57,6 +79,6 @@ for i in range(len(data)):
 
 from astropy.table import Table
 observed_ratios=Table([colratios,ratio_obs])
-observed_ratios.write('/Users/anshumanacharya/Downloads/c17.01/source/pyprog'+'{:.0f}'.format(UVB_Q)+'/Final/obs_ratios.txt', format='ascii.tab',overwrite='True')
+observed_ratios.write('/pyprog'+'{:.0f}'.format(UVB_Q)+'/Final/obs_ratios.txt', format='ascii.tab',overwrite='True')
 
 print("Done")
