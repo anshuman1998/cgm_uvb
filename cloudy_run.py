72714e18913f8e7f45c1d659e3930a611275b522
Vikram <vickykhaire@gmail.com>
2020-11-10 11:51:30 +0530

    adding the input file writing routine

--- cloudy_run.py
+++ cloudy_run.py
@@ -32,6 +32,21 @@ def run(cloudy_path, input_file):
 
 def write_input(file_name, *args, **kwargs):
 
+    """
+    :param file_name: the input filename where cloudy commands will be written
+    :param args: these will be ions to store
+    :param kwargs: the dict with keywords
+        :keyword uvb (if KS18 then): uvb_scale, uvb_Q, z,
+            hden_vary (True/False): if False give log_hden value
+                else give log_hden1, log_hden2, log_hden_step
+            log_metal,
+            scale_He,
+            stop_logNHI,
+            constant_T,
+            out_file_ext
+    :return:
+    """
+
     f = open(file_name, "w+")
 
     if kwargs['uvb'] == 'KS18':
@@ -39,7 +54,6 @@ def write_input(file_name, *args, **kwargs):
             kwargs['z'], kwargs['uvb'], kwargs['uvb_scale'], kwargs['uvb_Q'])
         f.write(uvb_statement)
 
-    density_statement= 'hden {} \n'.format(kwargs['log_hden'])
     if kwargs['hden_vary'] == True:
         density_statement= 'hden {} vary \n'.format(kwargs['log_hden'])
         variation_statement = 'grid range from {} to {} with {} dex step \n'.format(
@@ -47,6 +61,7 @@ def write_input(file_name, *args, **kwargs):
         f.write(density_statement)
         f.write(variation_statement)
     else:
+        density_statement = 'hden {} \n'.format(kwargs['log_hden'])
         f.write(density_statement)
 
     metal_statement =  'metals {:.2f} log \n'.format(kwargs['log_metal'])
@@ -83,12 +98,32 @@ def write_input(file_name, *args, **kwargs):
 
 
 # this is the part one needs to change if one wants to change the cloudy program
-def cloudy_params_defaults(z=0.2, T = 10000, metal = -1, stop_NHI = 14):
-    default_things ={}
-    
+def cloudy_params_defaults(uvb_Q, log_hden, hden_vary=True, uvb = 'KS18', z=0.2, T = 10000, metal = -1, stop_NHI = 14):
+
+    cloudy_params = {'uvb': uvb, 'z' : z, 'uvb_scale': 1, 'uvb_Q' : uvb_Q,
+                     'hden_vary' : hden_vary,
+                     'log_metal': metal,
+                     'const_T': T,
+                     'stop_logNHI': stop_NHI,
+                     'scale_He': 0.081632653}
+
+    if hden_vary :
+        cloudy_params['log_hden1'] = log_hden[0]
+        cloudy_params['log_hden2'] = log_hden[1]
+        cloudy_params['log_hden_step'] = log_hden[2]
+    else:
+        cloudy_params['log_hden'] = log_hden[0]
 
 
-    return ions, default_things
+    ions = {"H", "H+",
+            "He", "He+", "He++",
+            "C", "C+", "C+2", "C+3", "C+4", "C+5",
+            "N", "N+", "N+2", "N+3", "N+4",
+            "O", "O+", "O+2", "O+3", "O+4", "O+5", "O+6", "O+7",
+            "S", "S+", "S+2", "S+3", "S+4", "S+5",
+            "Si", "Si+", "Si+2", "Si+3", "Si+4"}
+
+    return ions, cloudy_params
 
 """
 UVB_Q = 20
