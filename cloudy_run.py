cf3e47471398fd1ab4fcbf8d549fb73439a8da5d
Vikram <vickykhaire@gmail.com>
2020-11-10 13:01:26 +0530

    testing

--- cloudy_run.py
+++ cloudy_run.py
@@ -78,12 +78,16 @@ def write_input(file_name, *args, **kwargs):
         temp_statement =  'constant temperature, t={} K [linear] \n'.format(kwargs['constant_T'])
         f.write(temp_statement)
 
+    # new line
+    save_hydrogen = 'save hydrogen conditions \"{.hydro}\" no clobber \n'
+    f.write(save_hydrogen)
+
     if 'out_file_ext' in kwargs.keys():
         out_file_extension = kwargs['out_file_ext']
     else:
         out_file_extension = '.spC'
 
-    save_statement = 'save species column density {} no hash \n'.format(out_file_extension)
+    save_statement = 'save species column density \"{}\" no hash \n'.format(out_file_extension)
     f.write(save_statement)
 
     for ion in args:
@@ -125,33 +129,9 @@ def cloudy_params_defaults(uvb_Q, log_hden, hden_vary=True, uvb = 'KS18', z=0.2,
 
     return ions, cloudy_params
 
-"""
-UVB_Q = 20
-dirname = '/home/vikram/cloudy_run/final'
-run = '/home/vikram/c17.02/source/cloudy.exe'
-hden = np.arange(-5, -2.999, 1)  # Hydrogen density grid
-z = -1  # in log scale
-stcolden = 14
-if not os.path.exists(dirname):
-    os.makedirs(dirname)
 
-os.chdir(dirname)
-
-filena = dirname + '/prog' + '{:.0f}'.format(UVB_Q) + '_Oct5.in'
-f = open(filena, "w+")
-f.write("TABLE KS18 redshift = 0.2 [scale= 1][Q=" + "{:.0f}".format(UVB_Q)
-        + "] \nhden -4 vary \ngrid range from -5 to -3 with 1 dex steps \nmetals "
-        + "{:.2f}".format(z) + " log \nelement helium abundance 0.081632653 linear \nstop column density "
-        + "{:.1f}".format(
-    stcolden) + " neutral H \nconstant temperature, t=1e4 K [linear] \nsave species column density \".spC\" no hash \n\"C+\" \n\"C+2\" \n\"C+3\" \n\"N+\" \n\"N+2\" \n\"N+3\" \n\"N+4\" \n\"O\" \n\"O+\"  \n\"O+2\" \n\"O+3\" \n\"O+4\" \n\"O+5\" \n\"S+3\" \n\"S+4\" \n\"S+5\" \n\"Si+\" \n\"Si+2\" \n\"Si+3\" \nend")
-f.close()
-process = subprocess.Popen([run, "prog" + "{:.0f}".format(UVB_Q) + "_Oct5.in"],
-                           stdout=subprocess.PIPE)
-process.stdout.read()
-
-outfilename = filena.split('.in')[0] + '.spC'
-data_20 = np.genfromtxt(outfilename)
-
-
-print("Done!!")
-"""
+cloudy_path = '/home/vikram/c17.02
+input_File = '/home/vikram/cloudy_run/try.txt'
+ions, params = cloudy_params_defaults(uvb_Q=20, log_hden= [-5, -3, 1])
+write_input(input_File, *ions, **params)
+run(cloudy_path= cloudy_path, input_file= input_File)
