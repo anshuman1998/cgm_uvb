ced5e7b587bf57fb54cdf764e67620856f866eb3
Vikram <vickykhaire@gmail.com>
2020-11-18 22:04:04 +0530

    more runs

--- cgm_uvb/vikram_cloudy_run.py
+++ cgm_uvb/vikram_cloudy_run.py
@@ -1,3 +1,12 @@
+#------------------
+# for reducing  numpy threads
+import os
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+#-----------------
 from cgm_uvb.cloudy_run import write_input
 from cgm_uvb.cloudy_run import run
 from cgm_uvb.cloudy_run import store_table
@@ -8,10 +17,10 @@ import multiprocessing as mp
 def run_parallel(uvb_Q):
     # for vikram
     cloudy_path = '/home/vikram/c17.02'
-    input_File = '/home/vikram/cloudy_run/anshuman/try_Q{}.in'.format(uvb_Q)
+    input_File = '/home/vikram/cloudy_run/anshuman_NH16/try_Q{}.in'.format(uvb_Q)
 
     # write input file and run cloudy
-    ions, params = cloudy_params_defaults(uvb_Q=uvb_Q, log_hden=[-6, -2, 0.01])
+    ions, params = cloudy_params_defaults(uvb_Q=uvb_Q, log_hden=[-6, -2, 0.01], stop_NHI = 16)
     write_input(input_File, *ions, **params)
     run(cloudy_path=cloudy_path, input_file=input_File)
 
