7d6c26c754fd1f5eae7aba15e898fde51e602d04
Vikram <vickykhaire@gmail.com>
2020-11-19 21:49:31 +0530

    finalizing 2 param mcmc code

--- cgm_uvb/mcmc.py
+++ cgm_uvb/mcmc.py
@@ -38,31 +38,6 @@ def get_interp_func(model_Q, ions_to_use):
     return logf
 
 
-def interpolate_model(theta, model_Q, ions_to_use, reference_log_metal = -1.0):
-    lognH, logZ =  theta
-
-    number_of_ions = len(ions_to_use)
-
-    model = tab.Table.read(model_Q)
-    sorted_model = model[ions_to_use]
-    hden_array = np.array(model['hden'])
-
-    model_touple = ()
-    for j in range(number_of_ions):
-        model_touple += (sorted_model[ions_to_use[j]],)
-
-    # interpolating in log log scale
-    logf = interp1d (np.log10(hden_array), np.log10(model_touple), fill_value = 'extrapolate')
-
-    # get metal ion column density for n_H and Z = 0.1
-    col = 10**logf(lognH)
-
-    # scale the column densities by the metallicity Z
-    metal_scaling_linear = 10**logZ / 10 ** reference_log_metal
-    model_ion_col = col * metal_scaling_linear
-
-    return model_ion_col
-
 #----for mcmc
 def log_likelihood(theta, interp_logf, obs_ion_col, col_err, reference_log_metal = -1.0):
     """
@@ -98,10 +73,10 @@ def log_posterior(theta, interp_func, data_col, sigma_col):
     return log_p
 
 
-def run_mcmc(model_Q, ions_to_use, true_Q =18 ):
+def run_mcmc(model_Q, ions_to_use, true_Q =18, figname = 'test.pdf'):
+    # run_mcmc(model_Q= model, ions_to_use= ions)
     # ------------------ here is a way to run code
     truths = [-4, -1]  # (lognH, logZ) true values
-    ions = ions_to_use
     number_of_ions = len(ions_to_use)
 
     data_col_all = get_true_model(model_Q, Q=true_Q)
@@ -110,10 +85,10 @@ def run_mcmc(model_Q, ions_to_use, true_Q =18 ):
     for name in ions_to_use:
         data_col.append(data_col_all[name][0])
 
-    sigma_col = 10**np.random.uniform(0.1, 0.5, number_of_ions)
-    sigma_col =10**0.4 * np.ones(number_of_ions)
-    print(data_col, sigma_col)
-
+    np.random.seed(0)
+    # sigma_col = np.random.uniform(0.1, 0.2, number_of_ions)
+    sigma_col = 0.2 * np.ones(number_of_ions)
+    print(np.log10(data_col), sigma_col)
 
     interp_logf = get_interp_func(model_Q, ions_to_use)
 
@@ -122,96 +97,74 @@ def run_mcmc(model_Q, ions_to_use, true_Q =18 ):
     # be nwalkers * nsteps
 
     ndim = 2  # number of parameters in the model
-    nwalkers = 100  # number of MCMC walkers
-    nsteps = 20000  # number of MCMC steps to take
+    nwalkers = 50  # number of MCMC walkers
+    nsteps = 5000  # number of MCMC steps to take
 
     # set theta near the maximum likelihood, with
-    #np.random.seed(0)
-    starting_guesses = 1e-4 * np.random.randn(nwalkers, ndim)  # initialise at a tiny sphere
+    n_guess = np.random.uniform(-5, -2, nwalkers)
+    z_guess = np.random.uniform(-2, 1, nwalkers)
+    starting_guesses = np.vstack((n_guess, z_guess)).T  # initialise at a tiny sphere
 
     # Here's the function call where all the work happens:
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_posterior, args=(interp_logf, data_col, sigma_col))
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_posterior, args=(interp_logf, np.log10(data_col), sigma_col))
     sampler.run_mcmc(starting_guesses, nsteps, progress=True)
 
     # find out number of steps
     tau = sampler.get_autocorr_time()  # number of steps needed to forget the starting position
-    print(tau)
-    #thin = int(np.mean(tau) / 2)  # use this number for flattning the sample as done below
-    thin = 100
-    flat_samples = sampler.get_chain(discard=thin * 10, thin=thin, flat=True)
+    #print(tau)
+    thin = int(np.mean(tau) / 2)  # use this number for flattning the sample as done below
+    #thin = 100
+    flat_samples = sampler.get_chain(discard=thin * 20, thin= 5, flat=True)
     # we are discarding some initial steps roughly 5 times the autocorr_time steps
     # then we thin by about half the autocorrelation time steps for plotting => one does not have to do this step
 
     labels = ['log nH', 'log Z']
-    fig = corner.corner(flat_samples, labels=labels, truths=truths, quantiles=[0.16, 0.5, 0.84],
-        show_titles=True, title_kwargs={"fontsize": 12})
+    uvb_q= int((model_Q.split('try_Q')[-1]).split('.fits')[0])
 
-    fig.show()
+    if uvb_q == true_Q:
+        fig = corner.corner(flat_samples, labels=labels, truths=truths, quantiles=[0.16, 0.5, 0.84],
+            show_titles=True, title_kwargs={"fontsize": 12})
+    else:
+        fig = corner.corner(flat_samples, labels=labels, quantiles=[0.16, 0.5, 0.84],
+            show_titles=True, title_kwargs={"fontsize": 12})
+
+    fig.savefig(figname)
 
     for i in range(ndim):
         mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
         q = np.diff(mcmc)
         print(labels[i], '=', mcmc[1], q[0], q[1])
 
-    return
+
+    return flat_samples, ndim
+
 
 ions_to_use= ['C+3', 'N+3', 'Si+3', 'O+5', 'C+2']
-model_Q ='/home/vikram/cloudy_run/anshuman/try_Q14.fits'
 true_Q =18
-#run_mcmc(model_Q= model, ions_to_use= ions)
-# ------------------ here is a way to run code
-truths = [-4, -1]  # (lognH, logZ) true values
-ions = ions_to_use
-number_of_ions = len(ions_to_use)
+outpath = '/home/vikram/cloudy_run/figures'
 
-data_col_all = get_true_model(model_Q, Q=true_Q)
-# converting astropy table row to a list
-data_col = []
-for name in ions_to_use:
-    data_col.append(data_col_all[name][0])
+outfile = outpath + '/NH14_out.fits'
 
-np.random.seed(0)
-#sigma_col = np.random.uniform(0.1, 0.2, number_of_ions)
-sigma_col =0.2 * np.ones(number_of_ions)
-print(np.log10(data_col), sigma_col)
+uvb_array= [14, 15, 16, 17, 18, 19, 20]
+out_tab =  tab.Table()
+for uvb_q in uvb_array:
+    model_Q = '/home/vikram/cloudy_run/anshuman/try_Q{}.fits'.format(uvb_q)
+    name = model_Q.split('/')[-2] + '_' + (model_Q.split('/')[-1]).split('.fits')[0]
+    figname = outpath + '/' + name + '.pdf'
 
-interp_logf = get_interp_func(model_Q, ions_to_use)
+    flat_samples, ndim = run_mcmc(model_Q=model_Q, ions_to_use=ions_to_use, true_Q=true_Q, figname=figname)
+    out =[[uvb_q]]
+    for i in range(ndim):
+        mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
+        q = np.diff(mcmc)
+        out.append([mcmc[1]])
+        out.append([q[0]])
+        out.append([q[1]])
 
-# Here we'll set up the computation. emcee combines multiple "walkers",
-# each of which is its own MCMC chain. The number of trace results will
-# be nwalkers * nsteps
+    print(out)
+    t = tab.Table(out, names = ('Q', 'nH', 'n16', 'n84', 'Z', 'Z16', 'Z84'))
+    out_tab = tab.vstack((out_tab, t))
 
-ndim = 2  # number of parameters in the model
-nwalkers = 50  # number of MCMC walkers
-nsteps = 3000  # number of MCMC steps to take
 
-# set theta near the maximum likelihood, with
-n_guess = np.random.uniform(-5, -2, nwalkers)
-z_guess = np.random.uniform(-2, 1, nwalkers)
-starting_guesses = np.vstack((n_guess, z_guess)).T  # initialise at a tiny sphere
-
-# Here's the function call where all the work happens:
-sampler = emcee.EnsembleSampler(nwalkers, ndim, log_posterior, args=(interp_logf, np.log10(data_col), sigma_col))
-sampler.run_mcmc(starting_guesses, nsteps, progress=True)
-
-# find out number of steps
-tau = sampler.get_autocorr_time()  # number of steps needed to forget the starting position
-print(tau)
-# thin = int(np.mean(tau) / 2)  # use this number for flattning the sample as done below
-thin = 100
-flat_samples = sampler.get_chain(discard=thin * 10, thin=10, flat=True)
-# we are discarding some initial steps roughly 5 times the autocorr_time steps
-# then we thin by about half the autocorrelation time steps for plotting => one does not have to do this step
-
-labels = ['log nH', 'log Z']
-fig = corner.corner(flat_samples, labels=labels, truths=truths, quantiles=[0.16, 0.5, 0.84],
-    show_titles=True, title_kwargs={"fontsize": 12})
-
-fig.show()
-
-for i in range(ndim):
-    mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
-    q = np.diff(mcmc)
-    print(labels[i], '=', mcmc[1], q[0], q[1])
-
+out_tab.write(outfile, overwrite = True)
 
