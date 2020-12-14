8479b7adad212a7af3d3028f04747a7597fbb930
Vikram <vickykhaire@gmail.com>
2020-12-14 16:47:36 +0530

    plotting

--- cgm_uvb/paper_plots/summary.py
+++ cgm_uvb/paper_plots/summary.py
@@ -4,83 +4,89 @@ import astropy.table as tab
 import matplotlib.pyplot as plt
 import os
 
-# setting the figure
-font = {'family': 'serif', 'weight': 'normal', 'size': 14}
-plt.rc('font', **font)
-mpl.rcParams['axes.linewidth'] = 1.5
+def make_summary_plot(file_name_fits, lsf_file_name, fig_name, x1lim= [-4.4, -3.45], x2lim = [-1.35, -0.62], loglsf = False):
+    # setting the figure
+    font = {'family': 'serif', 'weight': 'normal', 'size': 14}
+    plt.rc('font', **font)
+    mpl.rcParams['axes.linewidth'] = 1.5
 
-out_fig_name = 'summary_photoionized.pdf'
-figure_size = [12, 5]
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(figure_size[0], figure_size[1]))
-plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0.05)
+    figure_size = [12, 5]
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(figure_size[0], figure_size[1]))
+    plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0.05)
 
-file_name  =  '/home/vikram/cloudy_run/figures/2D/NH15_metal_2D.fits'
-#file_name  =  '/home/vikram/cloudy_run/figures/hybrid/NH15_hybrid_logT500.fits'
+    d = tab.Table.read(file_name_fits)
+    dlsf = tab.Table.read(lsf_file_name)
 
-d =  tab.Table.read(file_name)
+    lab = 'MCMC'
+    num = np.arange(len(d['uvb']))
+    ax1.errorbar(d['nH'][:-1], num[:-1], xerr=[d['n16'][:-1], d['n84'][:-1]], ls='', marker='.', markersize=16,
+        color='b', elinewidth=2, alpha=0.7, label=lab)
+    lab = 'Least Squares'
+    if loglsf:
+        ax1.scatter(np.log10(dlsf['nH'][:-1]), num[:-1], marker = 'x', color ='r', linewidth = 2, s = 300,  alpha =0.7)
+    else:
+        ax1.scatter(dlsf['nH'][:-1], num[:-1], marker='x', color='r', linewidth=2, s=300, alpha=0.7)
+
+    lab = 'MCMC'
+    ax2.errorbar(d['Z'][:-1], num[:-1], xerr=[d['Z16'][:-1], d['Z84'][:-1]], ls='', marker='.', markersize=16,
+        color='b', elinewidth=2, alpha=0.7, label=lab)
+    lab = 'Least Squares'
+    if loglsf:
+        ax2.scatter(np.log10(dlsf['Z'][:-1]), num[:-1], marker = 'x', color ='r', linewidth = 2, s = 300,  alpha =0.7)
+    else:
+        ax2.scatter(dlsf['Z'][:-1], num[:-1], marker='x', color='r', linewidth=2, s=300, alpha=0.7)
+
+    ax1.axvline(x=-4, linestyle='--', color='k', alpha=0.7)
+    ax2.axvline(x=-1, linestyle='--', color='k', alpha=0.7)
+
+    # ax1.tick_params(axis='x', which='minor', bottom=False)
+    ax1.set_yticks(num[:-1])
+
+    uvb_column = ['Q14', 'Q15', 'Q16', 'Q17', 'Q18', 'Q19', 'Q20', 'P19', 'FG20', 'HM12']
+    ax1.set_yticklabels(uvb_column[:-1])
+    ax2.set_yticklabels([])
+
+    ax1.set_ylabel('UVB models')
+    ax1.set_xlabel(r'log n$_{\rm H}$ (cm$^{-3}$)')
+    ax2.set_xlabel(r'log Z (Z$_{\odot}$)')
+
+    ax1.set_xlim(x1lim[0], x1lim[1])
+    ax2.set_xlim(x2lim[0], x2lim[1])
+    # ax.set_xscale('log')
+    # ax.set_yscale('log')
+
+
+    # change the marker size manually for both lines
+    # lgnd.legendHandles[0]._legmarker.set_markersize(6)
+    # lgnd.legendHandles[1]._legmarker.set_markersize(100)
+    # n_level1 = 'z = {:0.1f}'.format(z)
+    # ax.annotate (n_level1, xy=(1e4, 2e-7), fontsize=12)
+    for ax in (ax1, ax2):
+
+        ax.legend(loc='best', fontsize=14)
+
+        # deco
+        ax.tick_params(direction='in', length=6, width=1.5)
+        ax.tick_params(direction='in', which='minor', length=3.5, width=1.5)
+        # ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+        ax.yaxis.set_ticks_position('both')
+        ax.xaxis.set_ticks_position('both')
+        # decorating the plot
+        for axis in ['top', 'bottom', 'left', 'right']:
+            ax.spines[axis].set_linewidth(1.5)
+
+    fig.savefig(fig_name, bbox_inches='tight')
+
+    return
+
+
 
 lsf_file_name = '/home/vikram/cloudy_run/figures/2D/NH15_log_lsf_out.fits'
-#lsf_file_name = '/home/vikram/cloudy_run/figures/hybrid/NH15_log_lsf_hybrid_T500.fits'
+file_name = '/home/vikram/cloudy_run/figures/2D/NH15_metal_2D.fits'
+out_fig_name = 'summary_photoionized.pdf'
+make_summary_plot(file_name_fits= file_name, lsf_file_name =lsf_file_name, fig_name= out_fig_name)
 
-dlsf = tab.Table.read(lsf_file_name)
-
-
-lab = 'MCMC'
-num = np.arange(len(d['uvb']))
-ax1.errorbar( d['nH'][:-1], num[:-1], xerr = [d['n16'][:-1], d['n84'][:-1]], ls = '', marker ='.', markersize = 16, color = 'b', elinewidth =2, alpha =0.7, label = lab)
-lab = 'Least Squares'
-ax1.scatter(dlsf['nH'][:-1], num[:-1], marker = 'x', color ='r', linewidth = 2, s = 300,  alpha =0.7, label = lab)
-#ax1.scatter(np.log10(dlsf['nH'][:-1]), num[:-1], marker = 'x', color ='r', linewidth = 2, s = 300,  alpha =0.7, label = lab)
-
-lab = 'MCMC'
-ax2.errorbar( d['Z'][:-1], num[:-1], xerr = [d['Z16'][:-1], d['Z84'][:-1]], ls = '', marker ='.', markersize = 16, color = 'b', elinewidth=2, alpha = 0.7,label = lab )
-lab = 'Least Squares'
-ax2.scatter(dlsf['Z'][:-1], num[:-1], marker = 'x', color ='r', linewidth = 2, s = 300,  alpha =0.7, label = lab)
-#ax2.scatter(np.log10(dlsf['Z'][:-1]), num[:-1], marker = 'x', color ='r', linewidth = 2, s = 300,  alpha =0.7, label = lab)
-
-ax1.axvline(x = -4, linestyle = '--', color = 'k', alpha = 0.7)
-ax2.axvline(x = -1, linestyle = '--', color = 'k', alpha = 0.7)
-
-#ax1.tick_params(axis='x', which='minor', bottom=False)
-ax1.set_yticks(num[:-1])
-
-uvb_column = ['Q14', 'Q15', 'Q16', 'Q17', 'Q18', 'Q19', 'Q20', 'P19', 'FG20', 'HM12']
-ax1.set_yticklabels (uvb_column[:-1])
-ax2.set_yticklabels([])
-
-
-ax1.set_ylabel('UVB models')
-ax1.set_xlabel(r'log n$_{\rm H}$ (cm$^{-3}$)')
-ax2.set_xlabel(r'log Z (Z$_{\odot}$)')
-
-
-ax1.set_xlim(-4.45, -3.45)
-ax2.set_xlim(-1.45, -0.55)
-#ax.set_xscale('log')
-#ax.set_yscale('log')
-
-
-#change the marker size manually for both lines
-#lgnd.legendHandles[0]._legmarker.set_markersize(6)
-#lgnd.legendHandles[1]._legmarker.set_markersize(100)
-#n_level1 = 'z = {:0.1f}'.format(z)
-#ax.annotate (n_level1, xy=(1e4, 2e-7), fontsize=12)
-for ax in (ax1, ax2):
-
-    ax.legend(loc='best', fontsize=14)
-
-    # deco
-    ax.tick_params(direction='in', length=6, width=1.5)
-    ax.tick_params(direction='in', which='minor', length=3.5, width=1.5)
-    # ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
-    ax.yaxis.set_ticks_position('both')
-    ax.xaxis.set_ticks_position('both')
-    # decorating the plot
-    for axis in ['top', 'bottom', 'left', 'right']:
-        ax.spines[axis].set_linewidth(1.5)
-
-
-
-
-
-fig.savefig(out_fig_name, bbox_inches='tight')
+file_name  =  '/home/vikram/cloudy_run/figures/hybrid/NH15_hybrid_logT500.fits'
+lsf_file_name = '/home/vikram/cloudy_run/figures/hybrid/NH15_log_lsf_hybrid_T500.fits'
+out_fig_name = 'summary_hybrid.pdf'
+make_summary_plot(file_name_fits= file_name, lsf_file_name =lsf_file_name, fig_name= out_fig_name, loglsf= True)
