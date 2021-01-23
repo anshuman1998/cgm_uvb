ad49c48d4c851ab7ca21017abcbd4906682795d6
Vikram <vickykhaire@gmail.com>
2021-01-24 01:12:18 +0530

    getting the numbers

---
+++ cgm_uvb/paper_numbers.py
@@ -0,0 +1,48 @@
+import astropy.table as tab
+import numpy as np
+
+def find_max_numbers(filename):
+    print('for file: ', filename)
+
+    data =  tab.Table.read(filename)
+    # check if files are in log
+    if data ['nH'][0] < -1:
+        log = True
+    else:
+        log = False
+
+    max_n_mod =  data['uvb'][np.argmax(data['nH'])]
+    min_n_mod =  data['uvb'][np.argmin(data['nH'])]
+    if log :
+        diff_n_all  = np.max(data ['nH']) - np.min(data ['nH'])
+        diff_n_ks  = data ['nH'][0] - data ['nH'][6]
+    else:
+        print('-- lin')
+        data['nH'] = np.log10(data['nH'])
+        diff_n_all  = np.max(data ['nH']) - np.min(data ['nH'])
+        diff_n_ks  = data ['nH'][0] - data ['nH'][6]
+
+    print('delta nH = ', diff_n_all, 'for models:', max_n_mod, '-', min_n_mod)
+    print('delta nH = ', diff_n_ks, 'for KS models only:',  data['uvb'][0], '-', data['uvb'][6])
+
+
+
+    max_n_mod =  data['uvb'][np.argmax(data['Z'])]
+    min_n_mod =  data['uvb'][np.argmin(data['Z'])]
+    if log :
+        diff_n_all  = np.max(data ['Z']) - np.min(data ['Z'])
+        diff_n_ks  = data ['Z'][0] - data ['Z'][6]
+
+    else:
+        data['Z'] = np.log10(data['Z'])
+        diff_n_all  = np.max(data ['Z']) - np.min(data ['Z'])
+        diff_n_ks  = data ['Z'][0] - data ['Z'][6]
+
+    print('delta Z = ', diff_n_all, 'for models:', max_n_mod, '-', min_n_mod)
+    print('delta Z = ', diff_n_ks, 'for KS models only:', data['uvb'][0], '-', data['uvb'][6])
+
+    return
+
+
+path = '/home/vikram/cloudy_run/figures/hybrid'
+find_max_numbers(path + '/NH15_log_lsf_hybrid_T500.fits')
