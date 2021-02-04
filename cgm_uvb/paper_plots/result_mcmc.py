a8497fa4d96c5dda25a37f23e9d24705f11d92fb
Vikram <vickykhaire@gmail.com>
2021-02-04 11:55:56 +0530

    plotting

--- cgm_uvb/paper_plots/result_mcmc.py
+++ cgm_uvb/paper_plots/result_mcmc.py
@@ -11,21 +11,27 @@ for Q, uvb in zip(Q_array, uvb_array):
     flat_sample = np.load(file)
 
     labels = [r'log n$_{\rm H}$ (cm$^{-3}$)', r'log Z (Z$_{\odot}$)']
-    fig = corner.corner(flat_sample, labels=labels, quantiles=[0.16, 0.5, 0.84], show_titles=True, title_kwargs={"fontsize": 12})
+    truths = [-4, -1]
+    fig = corner.corner(flat_sample, labels=labels, quantiles=[0.16, 0.5, 0.84],
+                        show_titles=True, title_kwargs={"fontsize": 12})
+    fig.set_size_inches(6.5, 6.5)
 
     ndim = 2
     axes = np.array(fig.axes).reshape((ndim, ndim))
+    uvb_name = uvb
     if uvb == 'KS18':
-        legend_name = 'KS19 (Q{})'.format(Q)
-    else:
-        legend_name = '{}'.format(uvb)
-    axes[1, 0].annotate(legend_name, xy=(0.03, 0.85), xycoords='axes fraction', fontsize=9)
+        uvb_name = 'KS19' + ' (Q{})'.format(Q)
+    axes[1, 0].annotate(uvb_name, xy=(0.6, 0.04), xycoords='axes fraction', fontsize=11)
+    text_w = 'Photoionized absorber\n\n' + r'True (log n$_{\rm H}$, Z) = (-4, -1)' + '\n\nTrue UVB KS19 (Q18)'
+    axes[0, 1].annotate(text_w, xy=(0.08, 0.4), xycoords='axes fraction', fontsize=12)
+
 
     for ax in [axes[0, 0], axes[1, 0], axes[1, 1]]:
         ax.tick_params(direction='out', length=5, width=1.1)
         # ax.tick_params(direction='out', which='minor', length=3.5, width=1.5)
         for axis in ['top', 'bottom', 'left', 'right']:
             ax.spines[axis].set_linewidth(1.1)
+
 
     fig.savefig('result_{}_Q{}.pdf'.format(uvb, Q), bbox_inches='tight')
 
