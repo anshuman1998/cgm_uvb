9401e3cedc929f49ece066fb431e5ba09d43471a
Vikram <vickykhaire@gmail.com>
2021-02-04 11:55:12 +0530

    plotting

--- cgm_uvb/paper_plots/inference_test.py
+++ cgm_uvb/paper_plots/inference_test.py
@@ -12,11 +12,12 @@ labels = [r'log n$_{\rm H}$ (cm$^{-3}$)', r'log Z (Z$_{\odot}$)']
 truths = [-4, -1]
 fig = corner.corner(flat_sample, labels=labels, truths=truths, truth_color = 'r', quantiles=[0.16, 0.5, 0.84],
                     show_titles=True, title_kwargs={"fontsize": 12})
+fig.set_size_inches(6.5, 6.5)
 
 ndim = 2
 axes = np.array(fig.axes).reshape((ndim, ndim))
-axes[1,0].annotate ('KS19 (Q18)', xy=(0.03, 0.85), xycoords='axes fraction', fontsize=9)
-axes[0,1].annotate (r'Photoionized Model', xy=(0.08, 0.5), xycoords='axes fraction', fontsize=11)
+axes[1,0].annotate ('KS19 (Q18)', xy=(0.6, 0.04), xycoords='axes fraction', fontsize=11)
+axes[0,1].annotate ('Photoionized \n'+ 'absorber', xy=(0.08, 0.5), xycoords='axes fraction', fontsize=12)
 
 for ax in [axes[0, 0], axes[1, 0], axes[1,1 ]]:
     ax.tick_params(direction='out', length=5, width=1.1)
@@ -40,12 +41,14 @@ labels = [r'log n$_{\rm H}$ (cm$^{-3}$)', r'log Z (Z$_{\odot}$)']
 truths = [-4, -1]
 fig = corner.corner(flat_sample, labels=labels, truths=truths, truth_color = 'r', quantiles=[0.16, 0.5, 0.84],
                     show_titles=True, title_kwargs={"fontsize": 12})
+fig.set_size_inches(6.5, 6.5)
+
 
 ndim = 2
 axes = np.array(fig.axes).reshape((ndim, ndim))
-axes[1,0].annotate ('KS19 (Q18)', xy=(0.03, 0.85), xycoords='axes fraction', fontsize=9)
-axes[0,1].annotate (r'Hybrid Model (${\rm T=10^{5.5}}$ K)', xy=(0.06, 0.5), xycoords='axes fraction', fontsize=11)
-
+axes[1,0].annotate ('KS19 (Q18)', xy=(0.6, 0.04), xycoords='axes fraction', fontsize=11)
+axes[0,1].annotate ('Warm-hot absorber\n' + r'(${\rm T=10^{5.5}}$ K)', xy=(0.08, 0.5), xycoords='axes fraction', fontsize=12)
+axes[1,0].set_ylim(-1.35, -0.72)
 
 for ax in [axes[0, 0], axes[1, 0], axes[1,1 ]]:
     ax.tick_params(direction='out', length=5, width=1.1)
