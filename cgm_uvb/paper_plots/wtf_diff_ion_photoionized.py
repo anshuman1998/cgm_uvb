ca654b0cb6429401b4e629dca8266bd9b5f047c3
Vikram <vickykhaire@gmail.com>
2021-02-13 05:21:52 +0530

    plotting

---
+++ cgm_uvb/paper_plots/wtf_diff_ion_photoionized.py
@@ -0,0 +1,175 @@
+import astropy.table as t
+import matplotlib as mpl
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import glob
+from matplotlib.ticker import ScalarFormatter
+from astropy.cosmology import Planck15 as planck
+import scipy.optimize as sciopt
+import astropy.table as tab
+
+
+# setting the figure
+font = {'family': 'serif', 'weight': 'normal', 'size': 14}
+plt.rc('font', **font)
+mpl.rcParams['axes.linewidth'] = 1.5
+
+out_fig_name = 'wtf_only_dZ_new_diff_num_zoom.pdf'
+figure_size = [14, 4]
+fig, (ax1, ax2, ax3)  = plt.subplots(1, 3, figsize=(figure_size[0], figure_size[1]))
+plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0.15)
+
+
+path  = '/home/vikram/cloudy_run/diff_op/photo_NH15'
+
+uvb = ['KS18', 'HM12',  'P19', 'FG20']
+uvb_Q = [14, 15, 16, 17, 18, 19, 20]
+
+uvb_models =[]
+the_Q_values = []
+for background in uvb:
+    if background == 'KS18':
+        for q in uvb_Q:
+            uvb_models.append(background)
+            the_Q_values.append(q)
+    else:
+        q = 18
+        uvb_models.append(background)
+        the_Q_values.append(q)
+
+#ks_array = ['14', '15', '16', '17', '18', '19', '20']
+#all_uvb = ks_array + ['FG20', 'P19']
+#n_H_array = [1e-3, 1e-4, 1e-5]
+
+
+
+"""
+for num in ion_num:
+    d = tab.Table.read(path + '/diff_res_{}ions_new.txt'.format(num), format='ascii')
+    print(num,  len(d))
+    for m, n, Z, in zip(d['Q'], d['Max_diff_nH'], d['Max_diff_Z']):
+        for uvb in all_uvb:
+            if uvb == m:
+                n2_all.append(n)
+                z2_all.append(Z)
+
+print(len(n2_all))
+ax.scatter(n2_all, z2_all, alpha = 0.5, color = 'b')
+"""
+
+den = 1e-4
+met = -1
+ion_num = [2, 4, 6,  8]
+d = tab.Table.read(path + '/all_combined.fits')
+
+color_list = ['dodgerblue', 'orange', 'green', 'magenta']
+
+
+
+for num, col in zip(ion_num, color_list):
+    dnum = d[d['n_ions'] == num]
+    sort_d = dnum[dnum['true_nH'] == den]
+    sort_d = sort_d[sort_d['true_logZ'] == met]
+    #sort_d = sort_d[sort_d['z_dmax']>1.5]
+
+    #print(sort_d)
+
+
+    n_max_array = sort_d['n_dmax_KS']
+    z_max_array = sort_d['z_dmax_KS']
+
+    ax1.scatter(n_max_array, z_max_array, alpha=0.5, label='{} ions'.format(num), s=13)
+
+
+    binwidth = 0.02
+
+    ax2.hist(n_max_array, bins=np.arange(min(n_max_array), max(n_max_array) + binwidth, binwidth), alpha = 0.75,
+             histtype = 'bar', edgecolor= 'k', linewidth = 1.5, density = 1, label = '{} ions'.format(num) )
+
+
+    ax3.hist(z_max_array, bins=np.arange(min(z_max_array), max(z_max_array) + binwidth, binwidth), alpha = 0.75,
+             histtype = 'bar',  edgecolor= 'k', linewidth = 1.5, density = 1, label = '{} ions'.format(num))
+
+
+
+    """
+    #--------tasted but discarded
+    binwidth = 0.025
+
+    p = ax2.hist(n2_all_new, bins=np.arange(min(n2_all_new), max(n2_all_new) + binwidth, binwidth)-binwidth/2, alpha=0.35,
+                histtype='stepfilled', linewidth=2, density=1, edgecolor ='None',
+                label='' ,
+                facecolor=col)
+
+
+    p = ax2.hist(n2_all_new, bins=np.arange(min(n2_all_new), max(n2_all_new) + binwidth, binwidth)-binwidth/2, alpha=0.99,
+                histtype='step', linewidth=2, density=1, edgecolor =col,
+                label='')
+
+
+    p = ax3.hist(z2_all_new, bins=np.arange(min(z2_all_new), max(z2_all_new) + binwidth, binwidth)-binwidth/2, alpha=0.35,
+                histtype='stepfilled', linewidth=2, density=1, edgecolor ='None',
+                label='' ,
+                facecolor=col)
+
+    p = ax3.hist(z2_all_new, bins=np.arange(min(z2_all_new), max(z2_all_new) + binwidth, binwidth)-binwidth/2, alpha=0.99,
+                histtype='step', linewidth=2, density=1, edgecolor =col,
+                label='' )
+    
+    """
+
+    print(np.median(n_max_array), np.median(z_max_array), 'ions', num)
+
+
+
+ax1.annotate ('Photoionized \n' + 'absorber' , xy=(0.06, 0.65), xycoords='axes fraction', fontsize=12)
+
+ax1.annotate (r'True (log Z, log n$_{\rm H}$) = (-1, -4)' , xy=(0.06, 0.77), xycoords='axes fraction', fontsize=12)
+
+n_med = np.median(n_max_array)
+z_med = np.median(z_max_array)
+
+ax2.axvline(n_med, linestyle = '--', color = 'cyan')
+ax3.axvline (z_med, linestyle = '--', color = 'cyan')
+
+ax2.annotate (r'Median' , xy=(0.6, 0.9), xycoords='axes fraction', fontsize=12)
+ax2.annotate (r'$\Delta_{\rm max}$ (log n$_{\rm H}$) =' + '{:.2f}'.format(n_med) , xy=(0.6, 0.83), xycoords='axes fraction', fontsize=11)
+
+
+ax3.annotate (r'Median' , xy=(0.6, 0.9), xycoords='axes fraction', fontsize=12)
+ax3.annotate (r'$\Delta_{\rm max}$ (log Z) =' + '{:.2f}'.format(z_med) , xy=(0.6, 0.83), xycoords='axes fraction', fontsize=11)
+
+
+print(n_med, z_med)
+
+ax1.legend(loc = 'best',  fontsize = 12, ncol=2)
+
+ax1.set_xlabel(r'$\Delta_{\rm max}$ log n$_{\rm H}$ (cm $^{-3}$)')
+ax1.set_ylabel(r'$\Delta_{\rm max}$ log Z(Z$_{\odot}$)')
+ax1.set_xlim (0.1, 1.3)
+ax1.set_ylim (0.1, 1.3)
+ax2.set_xlim (0.2, 1.1)
+ax3.set_xlim (0.2, 1.1)
+
+ax2.set_xlabel(r'$\Delta_{\rm max}$ log n$_{\rm H}$ (cm $^{-3}$)')
+ax3.set_xlabel(r'$\Delta_{\rm max}$ log Z(Z$_{\odot}$)')
+ax2.set_ylabel('Frequency')
+#ax.set_xscale('log')
+#ax.set_yscale('log')
+
+
+#ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+
+for ax in (ax1, ax2, ax3):
+    # deco
+    ax.tick_params(direction='in', length=7, width=1.7)
+    ax.tick_params(direction='in', which='minor', length=4, width=1.7)
+    ax.yaxis.set_ticks_position('both')
+    ax.xaxis.set_ticks_position('both')
+    # decorating the plot
+    for axis in ['top', 'bottom', 'left', 'right']:
+        ax.spines[axis].set_linewidth(1.7)
+        ax.spines[axis].set_color('k')
+
+fig.savefig(out_fig_name, bbox_inches='tight')
