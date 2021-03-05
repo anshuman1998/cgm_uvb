560c69c3d4c73760c1a73f562d81071048481e7d
Vikram <vickykhaire@gmail.com>
2021-03-05 11:49:51 +0530

    changes

--- cgm_uvb/delta_sort.py
+++ cgm_uvb/delta_sort.py
@@ -141,6 +141,102 @@ def find_med_all( true_nH = 1e-4, true_logZ = -1,
 
 
 
+def find_med_all_hybrid( true_nH = 1e-4, true_logZ = -1,
+             table_file ='/home/vikram/cloudy_run/diff_op/hybrid_NH15/all_combined_logT550.fits', with_Ne8 = True ):
+
+
+
+    uvb_array = ['KS18',  'P19', 'FG20']
+    uvb_Q = [14, 15, 16, 17, 18, 19, 20]
+    uvb_models = []
+    the_Q_values = []
+    for background in uvb_array:
+        if background == 'KS18':
+            for q in uvb_Q:
+                uvb_models.append(background)
+                the_Q_values.append(q)
+        else:
+            q = 18
+            uvb_models.append(background)
+            the_Q_values.append(q)
+
+    nion_array = [ 8]
+
+    data = tab.Table.read(table_file)
+    data = data [data['true_logZ'] == true_logZ]
+    data = data [data ['true_nH'] == true_nH]
+
+    ions_used_coloum = data['ions']
+    flag = []
+    for i in range(len(data)):
+        ions = ions_used_coloum[i].split('_')
+        ne8 = False
+        for j in ions:
+            if j == 'Ne+7':
+                ne8 = True
+        if ne8 == True:
+            flag.append(1)
+        else:
+            flag.append(0)
+
+    flag = np.array(flag)
+
+    if with_Ne8:
+        data = data[flag == 1]
+    else:
+        data = data[flag == 0]
+
+    full_z = []
+    full_n = []
+
+    del_n_array = []
+    del_z_array = []
+
+    for true_uvb, true_Q in zip(uvb_models, the_Q_values):
+        #data_sort = tab.Table()
+        data_sort_in = data[data['true_uvb'] == true_uvb]
+        data_sort_in = data_sort_in[data_sort_in['true_Q'] == true_Q]
+
+        for nion in nion_array:
+            data_sort = data_sort_in[data_sort_in['n_ions'] == nion]
+
+            ks_nH = []
+            ks_Z = []
+            all_nH = []
+            all_Z = []
+
+
+            #print(len(data_sort), ': number of points')
+
+            for uvb, q_val in zip(uvb_models, the_Q_values):
+                col_name_nH = 'nH_' + uvb + '_Q{}'.format(q_val)
+                col_name_Z = 'Z_' + uvb + '_Q{}'.format(q_val)
+
+                full_n.append(np.median(data_sort[col_name_nH]))
+                full_z.append(np.median(data_sort[col_name_Z]))
+
+                all_nH.append(np.median(data_sort[col_name_nH]))
+                all_Z.append(np.median(data_sort[col_name_Z]))
+
+                if uvb == 'KS18':
+                    ks_nH.append(np.median(data_sort[col_name_nH]))
+                    ks_Z.append(np.median(data_sort[col_name_Z]))
+
+
+            del_n_ks = np.max(ks_nH) - np.min(ks_nH)
+            del_z_ks = np.max(ks_Z) - np.min(ks_Z)
+            del_n_all = np.max(all_nH) - np.min(all_nH)
+            del_z_all = np.max(all_Z) - np.min((all_Z))
+            del_n_array.append(del_n_all)
+            del_z_array.append(del_z_all)
+
+            #print('for true', true_uvb, true_Q, 'KS', del_n_ks, del_z_ks, 'nion:', nion)
+            #print('for true', true_uvb, true_Q, 'AL', del_n_all, del_z_all, 'nion:', nion)
+
+    return full_n, full_z, del_n_array, del_z_array
+
+
+
 def make_plot_photoionized(figname, outpath = '/home/vikram/cgm_uvb/cgm_uvb/paper_plots'):
 
     # setting the figure
@@ -162,6 +258,10 @@ def make_plot_photoionized(figname, outpath = '/home/vikram/cgm_uvb/cgm_uvb/pape
             x = np.median((np.array(dn) / 2))
             y = np.median((np.array(dz) / 2))
             print(x, y)
+            x1 = np.mean((np.array(dn) / 2))
+            y1 = np.mean((np.array(dz) / 2))
+            print('<>:', x1, y1)
+
             ax.errorbar([np.log10(nn)], [zz], xerr= x, yerr= y, marker ='.', markersize= 12, capsize = 5,  elinewidth = 2,
                         markeredgewidth=2, color = 'b')
                         #mec = 'k',  mfc= 'k', alpha=0.9)
@@ -184,6 +284,102 @@ def make_plot_photoionized(figname, outpath = '/home/vikram/cgm_uvb/cgm_uvb/pape
 
 
     #--------------------------------------------
+    # deco
+    ax.tick_params(direction='in', length=7, width=1.7)
+    ax.tick_params(direction='in', which='minor', length=4, width=1.7)
+    # ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+    ax.yaxis.set_ticks_position('both')
+    ax.xaxis.set_ticks_position('both')
+    # decorating the plot
+    for axis in ['top', 'bottom', 'left', 'right']:
+        ax.spines[axis].set_linewidth(1.7)
+        ax.spines[axis].set_color('k')
+
+    fig.savefig(out_fig_name, bbox_inches='tight')
+
+    return
+
+
+
+def make_plot_hybrid(figname, outpath = '/home/vikram/cgm_uvb/cgm_uvb/paper_plots'):
+
+    # setting the figure
+    font = {'family': 'serif', 'weight': 'normal', 'size': 14}
+    plt.rc('font', **font)
+    mpl.rcParams['axes.linewidth'] = 1.5
+
+    out_fig_name = outpath + '/' + figname
+    figure_size = [7, 6]
+    fig, ax = plt.subplots(1, 1, figsize=(figure_size[0], figure_size[1]))
+    #---------------------------------------------Plotting code
+    nH = [1e-5, 1e-4, 1e-3]
+    lgZ = [-2, -1, 0]
+
+    dummy = 0
+    alpha = 0.5
+    for nn in nH:
+        for zz in lgZ:
+
+            #----------------------------without ne8
+            colr = 'cyan'
+            n, z, dn, dz = find_med_all_hybrid(true_nH=nn, true_logZ=zz, with_Ne8= False)
+
+            x = np.median((np.array(dn) / 2))
+            y = np.median((np.array(dz) / 2))
+
+            if dummy == 0:
+                label = 'without Ne VIII'
+                ax.errorbar([np.log10(nn)], [zz], xerr= x, yerr= y, marker ='.', markersize= 12, capsize = 5,  elinewidth = 2,
+                            markeredgewidth=2, color = colr, label = label, alpha = alpha)
+                        #mec = 'k',  mfc= 'k', alpha=0.9)
+            else:
+                ax.errorbar([np.log10(nn)], [zz], xerr= x, yerr= y, marker ='.', markersize= 12, capsize = 5,  elinewidth = 2,
+                            markeredgewidth=2, color = colr, alpha = alpha)
+
+            txt = '({:.2f}, {:.2f})'.format(x, y)
+            ax.annotate(txt, (np.log10(nn)+0.1, zz-0.2), fontsize=10, color= colr)
+
+            #---------------------------- with Ne8
+            n, z, dn, dz = find_med_all_hybrid(true_nH=nn, true_logZ=zz)
+
+            x = np.median((np.array(dn) / 2))
+            y = np.median((np.array(dz) / 2))
+            print(x, y)
+            x1 = np.mean((np.array(dn) / 2))
+            y1 = np.mean((np.array(dz) / 2))
+            print('<>:', x1, y1)
+            if dummy == 0:
+                label = 'with Ne VIII'
+                ax.errorbar([np.log10(nn)], [zz], xerr= x, yerr= y, marker ='.', markersize= 12, capsize = 5,  elinewidth = 2,
+                            markeredgewidth=2, color = 'b', label = label, alpha = alpha)
+                        #mec = 'k',  mfc= 'k', alpha=0.9)
+            else:
+                ax.errorbar([np.log10(nn)], [zz], xerr= x, yerr= y, marker ='.', markersize= 12, capsize = 5,  elinewidth = 2,
+                            markeredgewidth=2, color = 'b', alpha = alpha)
+
+            txt = '({:.2f}, {:.2f})'.format(x, y)
+            ax.annotate(txt, (np.log10(nn)+0.1, zz+0.14), fontsize=10, color='b')
+
+
+            dummy +=1
+
+
+
+
+
+    ax.legend(loc=4, fontsize=12, ncol=2, handlelength=1)
+    #n_level1 = 'z = {:0.1f}'.format(z)
+    ax.annotate(r'Hybrid (10$^{5.5}$ K) absorbers ( $\Delta_{\rm max}$(log n$_{\rm H}$)/2,  $\Delta_{\rm max}$(log Z)/2 )',
+                xy=(-5.4, 0.466), fontsize=12)
+
+    ax.set_xlabel(r'log n$_{\rm H}$ (cm $^{-3}$)')
+    ax.set_ylabel(r'log Z(Z$_{\odot}$)')
+
+    ax.set_xlim(-5.55, -2.3)
+    ax.set_ylim(-2.7, 0.7)
+
+
+    #-------------------------------------------iiiiii-
     # deco
     ax.tick_params(direction='in', length=7, width=1.7)
     ax.tick_params(direction='in', which='minor', length=4, width=1.7)
@@ -225,3 +421,4 @@ plt.show()
 
 
 make_plot_photoionized(figname='res_final_phot.pdf')
+make_plot_hybrid(figname='res_final_hybrid.pdf')
