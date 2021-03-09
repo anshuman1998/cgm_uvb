4e473d764c370db6bcda8f0d6969b0c64cf03207
Vikram <vickykhaire@gmail.com>
2021-03-09 12:01:56 +0530

    plot ion fractions

--- cgm_uvb/frac_plots.py
+++ cgm_uvb/frac_plots.py
@@ -39,33 +39,82 @@ def get_the_factions(filename, ion, metallicity):
 
     return den_array, frac_array
 
+def plot_ion_fractions_photoionized(path, ion, metallicity = 0.1, legend_txt = None, alpha= 0.5,
+                                    outpath = '/home/vikram/cloudy_run/more_fig/ion_fac'):
+    # setting the figure
+    font = {'family': 'serif', 'weight': 'normal', 'size': 13}
+    plt.rc('font', **font)
+    mpl.rcParams['axes.linewidth'] = 1.5
+
+    out_fig_name = outpath +'/photoionized_fraction_{}.jpg'.format(ion)
+    figure_size = [7, 6]
+    fig, ax = plt.subplots(1, 1, figsize=(figure_size[0], figure_size[1]))
+
+    uvb = ['KS18', 'HM12', 'P19', 'FG20']
+    uvb_Q = [14, 15, 16, 17, 18, 19, 20]
+
+    uvb_models = []
+    the_Q_values = []
+    for background in uvb:
+        if background == 'KS18':
+            for q in uvb_Q:
+                uvb_models.append(background)
+                the_Q_values.append(q)
+        else:
+            q = 18
+            uvb_models.append(background)
+            the_Q_values.append(q)
+
+    logZ = np.log10(metallicity)
+    fname = (logZ + 4) * 100
+
+    for uvb_name, q_name in zip(uvb_models, the_Q_values):
+        filename = path + '/try_{}_Q{}_Z{:.0f}.fits'.format(uvb_name, q_name, fname)
+        x, y = get_the_factions(filename = filename, ion= ion, metallicity= metallicity)
+
+        q_name = 'Q{}'.format(Q)
+        if uvb_name == 'KS18':
+            label = 'KS19 ({})'.format(q_name)
+        else:
+            label = uvb_name
+
+        ax.plot(np.log10(x), y, label=label, linewidth=3, alpha=alpha)
+
+    # 1 ryd = 13.6057 eV
+
+    ax.legend(loc='best', fontsize=12, ncol=2, handlelength=2.6)
+
+    if legend_txt is not None:
+        ax.annotate(legend_txt, xytext=(0.8, 0.9), textcoords='axes fraction', fontsize=12)
+
+
+    ax.set_ylabel('frac of {}'.format(ion))
+    ax.set_xlabel(r'log n$_{\rm H}$')
+    #ax.set_xlim(1, 100000)
+    #ax.set_ylim(4e-8, 3e-4)
+    #ax.set_xscale('log')
+    #ax.set_yscale('log')
+
+    # deco
+    ax.tick_params(direction='in', length=7, width=1.7)
+    ax.tick_params(direction='in', which='minor', length=4, width=1.7)
+    # ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+    ax.yaxis.set_ticks_position('both')
+    ax.xaxis.set_ticks_position('both')
+    # decorating the plot
+    for axis in ['top', 'bottom', 'left', 'right']:
+        ax.spines[axis].set_linewidth(1.7)
+        ax.spines[axis].set_color('k')
+
+    fig.savefig(out_fig_name, bbox_inches='tight')
+
+
 
 
 path = '/home/vikram/cloudy_run/metal_NH15_new'
+plot_ion_fractions_photoionized( path = path, ion= 'O+5')
 
-uvb = ['KS18', 'HM12',  'P19', 'FG20']
-uvb_Q = [14, 15, 16, 17, 18, 19, 20]
-
-uvb_models =[]
-the_Q_values = []
-for background in uvb:
-    if background == 'KS18':
-        for q in uvb_Q:
-            uvb_models.append(background)
-            the_Q_values.append(q)
-    else:
-        q = 18
-        uvb_models.append(background)
-        the_Q_values.append(q)
-
-
-logZ = -1
-fname = (logZ+4)*100
-
-for uvb_name, q_name in zip(uvb_models, the_Q_values):
-    filename = 'try_{}_Q{}_Z{:.0f}.fits'.format(uvb_name, q_name, fname)
-    file_with_path = path + '/' + filename
-    find_max_o6_frac(filename= file_with_path)
+"""
 
 logT = 5.5
 print('for hot gas')
@@ -76,3 +125,5 @@ for uvb_name, q_name in zip(uvb_models, the_Q_values):
     filename = 'try_{}_Q{}_logT{:.0f}.fits'.format(uvb_name, q_name, fname)
     file_with_path = path + '/' + filename
     find_max_o6_frac(filename= file_with_path)
+
+"""
