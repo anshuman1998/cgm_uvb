86d24c1d842ab6056d6359e430c1441bc80b6f85
Vikram <vickykhaire@gmail.com>
2021-03-09 11:27:04 +0530

    max o6 fraction

--- cgm_uvb/find_max_o6_frac.py
+++ cgm_uvb/find_max_o6_frac.py
@@ -5,7 +5,7 @@ def find_max_o6_frac(filename, Z = 0.1, log_o_abd = -3.3098):
 
     data  = tab.Table.read(filename)
     tot_o = (data['H']+ data['H+'])*Z*10**log_o_abd
-    frac_array = data['O+']/tot_o
+    frac_array = data['O+5']/tot_o
     max_arg =np.argmax(frac_array)
     print(frac_array[max_arg], data['hden'][max_arg], filename.split('/')[-1])
 
@@ -38,9 +38,12 @@ for uvb_name, q_name in zip(uvb_models, the_Q_values):
     file_with_path = path + '/' + filename
     find_max_o6_frac(filename= file_with_path)
 
+logT = 5.5
 print('for hot gas')
-
+path = '/home/vikram/cloudy_run/hybrid_NH15'
+fname = logT * 100
 for uvb_name, q_name in zip(uvb_models, the_Q_values):
-    filename = 'try_{}_Q{}_Z{:.0f}.fits'.format(uvb_name, q_name, fname)
+
+    filename = 'try_{}_Q{}_logT{:.0f}.fits'.format(uvb_name, q_name, fname)
     file_with_path = path + '/' + filename
     find_max_o6_frac(filename= file_with_path)
